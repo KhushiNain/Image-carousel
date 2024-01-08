b73a65f55ae48c9ce3915b8b1cc01de6e0e95bd7
KhushiNain <146444846+KhushiNain@users.noreply.github.com>
2024-01-08 17:13:54 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,6 +7,56 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            index:0
+        }
+    }
+
+    handleBackArr=()=>{
+        if(this.state.index==0){
+            this.setState({
+                index: images.length-1
+            })
+        }
+        else{
+            this.setState({ 
+                index:this.state.index-1
+            })
+        }
+        
+    }
+
+    handleForwardArr=()=>{
+        if(this.state.index==images.length-1){
+            this.setState({
+                index:0
+            })
+        }else {
+            this.setState({
+                index:this.state.index+1
+            })
+        }
+        
+    }
+
+
+
+    render(){
+        return(
+            <>
+            <img className="image" src={images[this.state.index].img} alt="image " />
+            <h1 className="heading text">{images[this.state.index].title}</h1>
+            <h2 className="text subheading">{images[this.state.index].subtitle}</h2>
+            
+            <ArrowBackIosIcon className="button left-btn" onClick={this.handleBackArr}/>
+            <ArrowForwardIosIcon className="button right-btn" onClick={this.handleForwardArr} />      
+            
+            </>
+        ) 
+    }
   
 }
 
